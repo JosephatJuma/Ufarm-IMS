73aa44e03f077956950ad21b9a16c892e665a5e0
Josephat10 <91407741+Josephat10@users.noreply.github.com>
2023-04-18 17:16:11 +0300

    Validations doe the user sign up

--- Routes/registerRoute.js
+++ Routes/registerRoute.js
@@ -4,5 +4,8 @@ const router = express.Router();
 router.get("/", (req, res) => {
   res.render("register.pug");
 });
+router.post("/", (req, res) => {
+  res.send(req.body);
+});
 
 module.exports = router;
---
+++ public/js/sign.js
@@ -0,0 +1,140 @@
+//Regular Expressions
+const nameREGEX =
+  /^([a-zA-Z]{2,}\s[a-zA-Z]{1,}'?-?[a-zA-Z]{2,}\s?([a-zA-Z]{1,})?)/;
+const passGEX =
+  /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@$#!%*?&])([a-zA-Z0-9@$#!%*?&]{8,16})$/;
+const emailREGEX =
+  /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+const phoneREGEX =
+  /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/;
+const ninRegex = /^[A-Z0-9]{14}$/;
+
+function validateUserForm() {
+  let input;
+  let err = "";
+  //Validate Name
+  let name = document.forms["regForm"]["name"].value;
+  input = document.getElementById("name");
+  err = document.getElementById("nameErr");
+  if (name == "") {
+    input.focus();
+    input.style.borderColor = "red";
+    err.textContent = "Name can't be empty";
+    return false;
+  } else {
+    const validName = nameREGEX.test(name);
+    if (!validName) {
+      input.focus();
+      input.style.borderColor = "red";
+      err.textContent = "Invalid name given";
+      return false;
+    }
+    err.textContent = "";
+    input.style.borderColor = "";
+  }
+  //Validate gender
+  let gender = document.forms["regForm"]["gender"].value;
+  input = document.getElementById("gender");
+  err = document.getElementById("genderErr");
+  if (!gender) {
+    err.textContent = "Gender not selected";
+    input.focus();
+    input.style.borderColor = "red";
+    return false;
+  }
+  err.textContent = "";
+  input.style.borderColor = "";
+
+  //Validate date of birth
+  let birthDate = document.forms["regForm"]["dob"].value;
+  err = document.getElementById("dobErr");
+  input = document.getElementById("dob");
+  if (!birthDate) {
+    input.focus();
+    input.style.borderColor = "red";
+    err.textContent = "Date of birth is needed";
+    return false;
+  }
+  err.textContent = "";
+  input.style.borderColor = "";
+
+  //Validate user address
+  let address = document.forms["regForm"]["address"].value;
+  err = document.getElementById("addressErr");
+  input = document.getElementById("address");
+  if (!address) {
+    err.textContent = "Address is required";
+    input.focus();
+    input.style.borderColor = "red";
+    return false;
+  }
+  err.textContent = "";
+  input.style.borderColor = "";
+  //Validate phone number
+  let phoneNumber = document.forms["regForm"]["phone"].value;
+  input = document.getElementById("phone");
+  err = document.getElementById("phoneErr");
+  if (!phoneNumber) {
+    input.focus();
+    input.style.borderColor = "red";
+    err.textContent = "Phone number is required";
+    return false;
+  } else {
+    const validPhone = phoneREGEX.test(phoneNumber);
+    if (!validPhone) {
+      input.focus();
+      input.style.borderColor = "red";
+      err.textContent = "Invalid phone number provided";
+      return false;
+    }
+    err.textContent = "";
+    input.style.borderColor = "";
+  }
+  //Validate email number
+  let email = document.forms["regForm"]["email"].value;
+  err = document.getElementById("emailErr");
+  input = document.getElementById("email");
+  if (!email) {
+    input.focus();
+    input.style.borderColor = "red";
+    err.textContent = "Email is needed";
+    return false;
+  }
+  if (email) {
+    const validPhone = emailREGEX.test(email);
+    if (!validPhone) {
+      input.focus();
+      input.style.borderColor = "red";
+      err.textContent = "Invalid email address provided";
+      return false;
+    }
+  } else {
+    err.textContent = "";
+    input.style.borderColor = "";
+  }
+
+  //Validate Password
+  let password = document.forms["regForm"]["password"].value;
+  input = document.getElementById("password");
+  err = document.getElementById("passwordErr");
+  if (!password) {
+    input.focus();
+    input.style.borderColor = "red";
+    err.textContent = "Password is required";
+    return false;
+  } else {
+    const validPassword = passGEX.test(password);
+    if (!validPassword) {
+      input.focus();
+      input.style.borderColor = "red";
+      err.textContent =
+        "Password must have atleast 1 uppercase, 1 lowercase letter, number, and speacial characters";
+      return false;
+    }
+    err.textContent = "";
+    input.style.borderColor = "";
+  }
+  err.textContent = "";
+  input.style.borderColor = "";
+  return true;
+}
