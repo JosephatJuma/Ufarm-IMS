ff437b9d3feabc2e0ba8bb1badece27b73d3ecb9
Josephat10 <91407741+Josephat10@users.noreply.github.com>
2023-04-27 12:21:08 +0300

    validations added for the assign password

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,31 +1,35 @@
 const passGEX =
   /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@$#!%*?&])([a-zA-Z0-9@$#!%*?&]{8,16})$/;
 
-const validateCredentailsForm = () => {
+function validateCredentailsForm() {
+  let input;
   let password = document.forms["credentials"]["password"].value;
-  let passErr = document.getElementById("passErr");
   let confirm = document.forms["credentials"]["confirm"].value;
-  let confirmErr = document.getElementById("confirmErr");
-  let validPassword = passGEX.test(password);
-  if (password === "") {
-    alert("No password");
-    passErr.textContent = "Password cannot be empty!";
+  let err = "";
+  input = document.getElementById("password");
+  err = document.getElementById("passErr");
+  input.style.borderColor = "";
+  if (password == "") {
+    input.style.borderColor = "red";
+    err.textContent = "Password can't be empty!";
     return false;
-  } else if (validPassword !== true) {
-    alert("set strong password");
-    passErr.textContent = "";
-    passErr.textContent =
-      "Invalid password, password must be a combination of different characters!";
-    return false;
-  }
-
-  if (confirm === "") {
-    confirmErr.textContent = "You must confirm the password";
+  } else if (passGEX.test(password) !== true) {
+    input.style.borderColor = "red";
+    err.textContent =
+      "Password must be a combination of different character and must be atleast 8 character long!";
     return false;
   }
-  if (confirm !== password) {
-    confirmErr.textContent = "Passwords do not match";
+  input = document.forms["credentials"]["confirm"];
+  if (confirm == "") {
+    input.style.borderColor = "red";
+    err.textContent = "You must confirm password!";
     return false;
+  } else if (confirm !== password) {
+    input.style.borderColor = "red";
+    err.textContent = "Passwords do not match!";
+    return false;
+  } else {
+    err.textContent = "";
   }
   return true;
-};
+}
